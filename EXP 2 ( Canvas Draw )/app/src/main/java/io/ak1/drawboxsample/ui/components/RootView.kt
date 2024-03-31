73b5a5c5bc741fd0dec04f2227464efa4c489485
Ishan K <159259003+VIRAJKHERDE24@users.noreply.github.com>
2024-03-31 14:55:13 +0530

    .

---
+++ EXP 2 ( Canvas Draw )/app/src/main/java/io/ak1/drawboxsample/ui/components/RootView.kt
@@ -0,0 +1,21 @@
+package io.ak1.drawboxsample.ui.components
+
+import android.view.Window
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import io.ak1.drawboxsample.ui.theme.DrawBoxTheme
+import io.ak1.drawboxsample.ui.theme.isSystemInDarkThemeCustom
+import io.ak1.drawboxsample.ui.theme.StatusBarConfig
+
+
+@Composable
+fun Root(window: Window, content: @Composable () -> Unit) {
+    val isDark = isSystemInDarkThemeCustom()
+    DrawBoxTheme(isDark) {
+        window.StatusBarConfig(isDark)
+        Surface(color = MaterialTheme.colors.background) {
+            content.invoke()
+        }
+    }
+}
